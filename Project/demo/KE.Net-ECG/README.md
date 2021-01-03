da221f1b7cde7c2ecf01d3c223b71ef011614e6b
Link <scyhc3@nottingham.edu.cn>
2021-01-03 20:41:43 +0800

    demo

---
+++ Project/demo/KE.Net-ECG/README.md
@@ -0,0 +1,11 @@
+# KE.Net ECG
+
+KE.Net ECG is an Android application that implements getting an ECG from a Polar H10 heart monitor. The H10 needs to have firmware version 3.0.35 or later installed, and you need to know the ID of your device (which is printed on its side).  You can see the ECG trace in real time and save PNG and CSV files of the data.
+
+It uses the Polar SDK located at  https://github.com/polarofficial/polar-ble-sdk.
+
+**More information**
+
+More information and FAQ are at https://kennethevans.github.io as well as more projects from the same author.
+
+Licensed under the MIT license. (See: https://en.wikipedia.org/wiki/MIT_License)
---
+++ Project/demo/KE.Net-ECG/app/src/main/java/net/kenevans/polar/polarecg/Plotter.java
@@ -0,0 +1,113 @@
+package net.kenevans.polar.polarecg;
+
+import android.util.Log;
+
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeriesFormatter;
+
+import polar.com.sdk.api.model.PolarEcgData;
+
+@SuppressWarnings("WeakerAccess")
+public class Plotter {
+
+    String title;
+    private String TAG = "Polar_Plotter";
+    private PlotterListener listener;
+    private Number[] plotNumbers = new Number[500];
+    private XYSeriesFormatter formatter;
+    private SimpleXYSeries series;
+    /**
+     * The next index in the data
+     */
+    private long dataIndex;
+    /**
+     * The number of points to show
+     */
+    private int dataSize;
+    /**
+     * The total number of points to keep
+     */
+    private int totalDataSize;
+
+
+    public Plotter(int totalDataSize, int dataSize,
+                   String title,
+                   Integer lineColor, boolean showVertices) {
+        this.title = title;
+        this.dataSize = dataSize;
+        this.totalDataSize = totalDataSize;
+        this.dataIndex = 0;
+
+        formatter = new LineAndPointFormatter(lineColor,
+                showVertices ? lineColor : null, null, null);
+        formatter.setLegendIconEnabled(false);
+
+        series = new SimpleXYSeries(title);
+    }
+
+    public SimpleXYSeries getSeries() {
+        return series;
+    }
+
+    public XYSeriesFormatter getFormatter() {
+        return formatter;
+    }
+
+    /**
+     * Implements a strip chart adding new data at the end.
+     *
+     * @param plot         The associated XYPlot.
+     * @param polarEcgData The data that came in.
+     */
+    public void addValues(XYPlot plot, PolarEcgData polarEcgData) {
+        Log.d(TAG,
+                "addValues: dataIndex=" + dataIndex + " seriesSize=" + series.size());
+        int nSamples = polarEcgData.samples.size();
+        if (nSamples == 0) return;
+
+        // Add the new values, removing old values if needed
+        for (Integer val : polarEcgData.samples) {
+            if (series.size() >= totalDataSize) {
+                series.removeFirst();
+            }
+            // Convert from  μV to mV
+            series.addLast(dataIndex++, .001 * val);
+            updatePlot(plot);
+//            Log.d(TAG, "addValues thread: " + Thread.currentThread()
+//            .getName());
+        }
+    }
+
+    public void updatePlot(XYPlot plot) {
+        long plotMin, plotMax;
+        if (dataIndex < dataSize) {
+            plotMin = dataIndex - dataSize;
+            plotMax = dataIndex;
+        } else {
+            plotMin = dataIndex - dataSize;
+            plotMax = dataIndex;
+        }
+        plot.setDomainBoundaries(plotMin, plotMax, BoundaryMode.FIXED);
+        listener.update();
+    }
+
+    /**
+     * Clear the plot and reset dataIndex;
+     */
+    public void clear() {
+        dataIndex = 0;
+        series.clear();
+        listener.update();
+    }
+
+    public void setListener(PlotterListener listener) {
+        this.listener = listener;
+    }
+
+    public long getDataIndex() {
+        return dataIndex;
+    }
+}

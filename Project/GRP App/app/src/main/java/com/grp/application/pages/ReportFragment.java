97ddd418e6b038b137368389ae33425a906807e2
xiaxiaotian <scyxx2@nottingham.edu.cn>
2021-02-23 16:52:02 +0800

    complete graph to show the data

--- Project/GRP App/app/src/main/java/com/grp/application/pages/ReportFragment.java
+++ Project/GRP App/app/src/main/java/com/grp/application/pages/ReportFragment.java
@@ -11,7 +11,11 @@ import androidx.fragment.app.Fragment;
 import com.grp.application.MainActivity;
 import com.example.application.R;
 import com.google.android.material.tabs.TabLayout;
+import com.grp.application.database.Dao;
 import com.grp.application.monitor.Monitor;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * {@code ReportFragment} is class to maintain UI elements and functions of report page.
@@ -22,11 +26,47 @@ import com.grp.application.monitor.Monitor;
 public class ReportFragment extends Fragment {
     private Monitor monitor;
     private MainActivity mainActivity;
+    private final int hours = 24;
+    private final int weekDays = 7;
+    private final int monthDays = 31;
+    private GraphView graphView;
+    private GraphView graphView2;
+    private GraphView graphView3;
+    private Dao dao;
 
     private TabLayout durationTab;
 
-
     public ReportFragment() {}
+
+    private DataPoint[] data(Number[] number, int num){
+        DataPoint[] values = new DataPoint[num];     //creating an object of type DataPoint[] of size 'n'
+        for(int i=0;i<num;i++){
+            DataPoint v = new DataPoint(i, number[i].doubleValue());
+            values[i] = v;
+        }
+        return values;
+    }
+
+    private DataPoint[] dailyData(){
+        Number[] number = dao.getDailyData();
+        DataPoint[] values;
+        values = data(number, hours);
+        return values;
+    }
+
+    private DataPoint[] weeklyData(){
+        Number[] number = dao.getWeeklyData();
+        DataPoint[] values;
+        values = data(number, weekDays);
+        return values;
+    }
+
+    private DataPoint[] monthlyData(){
+        Number[] number = dao.getMonthlyData();
+        DataPoint[] values;
+        values = data(number, monthDays);
+        return values;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +75,10 @@ public class ReportFragment extends Fragment {
         monitor = Monitor.getInstance();
         mainActivity = (MainActivity) getActivity();
         durationTab = root.findViewById(R.id.tab_duration);
+        graphView = (GraphView)root.findViewById(R.id.dailyGraphView);
+        graphView2 = (GraphView)root.findViewById(R.id.weeklyGraphView);
+        graphView3 = (GraphView)root.findViewById(R.id.monthlyGraphView);
+        dao = new Dao(mainActivity.getApplicationContext());
 
         durationTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 
@@ -54,6 +98,37 @@ public class ReportFragment extends Fragment {
             public void onTabReselected(TabLayout.Tab tab) {}
         });
 
+        startPlot1();
+        startPlot2();
+        startPlot3();
         return root;
     }
+
+    private void startPlot1(){
+        LineGraphSeries<DataPoint> series= new LineGraphSeries<>(dailyData());
+        series.setDrawDataPoints(true);
+        graphView.addSeries(series);
+        double max_x = 24.0;
+        graphView.getViewport().setXAxisBoundsManual(true);
+        graphView.getViewport().setMaxX(max_x);
+    }
+
+    private void startPlot2(){
+        LineGraphSeries<DataPoint> series= new LineGraphSeries<>(weeklyData());
+        series.setDrawDataPoints(true);
+        graphView2.addSeries(series);
+        double max_x = 7.0;
+        graphView2.getViewport().setXAxisBoundsManual(true);
+        graphView2.getViewport().setMaxX(max_x);
+    }
+
+    private void startPlot3(){
+        LineGraphSeries<DataPoint> series= new LineGraphSeries<>(monthlyData());
+        series.setDrawDataPoints(true);
+        graphView3.addSeries(series);
+        double max_x = 31.0;
+        graphView3.getViewport().setXAxisBoundsManual(true);
+        graphView3.getViewport().setMaxX(max_x);
+    }
+
 }
